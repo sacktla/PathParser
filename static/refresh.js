9afdf285eacc140eef53aba6e627f73d6b9ebe2d
Jesus Zaragoza <jesuszaragoza@Christys-MBP.attlocal.net>
2019-02-05 23:57:04 -0800

    Added static folder. Next update must include search by ind. fields

---
+++ static/refresh.js
@@ -0,0 +1,31 @@
+let study_select = document.getElementById('study')
+let category_select = document.getElementById('category')
+let value_select    = document.getElementById('value')
+
+study_select.onchange = function(){
+  study_id = study_select.value;
+  fetch('/category/' + study_id).then(function(response){
+    response.json().then(function(data){
+      let optionHTML = '';
+      for(let category of data.categories){
+        optionHTML += '<option value="' + category.id + '">' + category.category + '</option>';
+      }
+
+      category_select.innerHTML = optionHTML;
+    });
+  });
+}
+
+category_select.onchange = function(){
+  category_id = category_select.value;
+  fetch('/value/' + category_id).then(function(response){
+    response.json().then(function(data){
+      let optionHTML = '';
+      for (let value of data.values){
+        optionHTML += '<option value="' + value.id + '">' + value.value + '</option>';
+      }
+
+      value_select.innerHTML = optionHTML;
+    });
+  });
+}
